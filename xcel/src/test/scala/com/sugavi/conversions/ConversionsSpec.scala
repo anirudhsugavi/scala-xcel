21a5db7cd5da782632ffc1a3cf9de7c17ad7ff31
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2025-06-13 22:20:13 +0530

    refactor ConversionsSpec to improve type checking and introduce XcelVals case class

--- xcel/src/test/scala/com/sugavi/conversions/ConversionsSpec.scala
+++ xcel/src/test/scala/com/sugavi/conversions/ConversionsSpec.scala
@@ -1,7 +1,6 @@
 package com.sugavi.conversions
 
 import com.sugavi.xcel.model.*
-import com.sugavi.xcel.syntax.given
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
@@ -24,26 +23,51 @@ class ConversionsSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyC
         optStr: Option[String],
         optDob: Option[Double]
       ) =>
-        val strX: XcelValue      = str
-        val dobX: XcelValue      = dob
-        val intX: XcelValue      = int
-        val longX: XcelValue     = long
-        val boolX: XcelValue     = bool
-        val dateX: XcelValue     = date
-        val dateTimeX: XcelValue = dateTime
-        val optStrX: XcelValue   = optStr
-        val optDobX: XcelValue   = optDob
+        import scala.compiletime.testing.*
 
-        val str1: String             = strX
-        val dob1: Double             = dobX
-        val int1: Int                = intX
-        val long1: Long              = longX
-        val bool1: Boolean           = boolX
-        val date1: LocalDate         = dateX
-        val dateTime1: LocalDateTime = dateTimeX
-        val optStr1: Option[String]  = optStrX
-        val optDob1: Option[Double]  = EmptyXcel
+        val errors = typeCheckErrors(
+          """
+            |import com.sugavi.xcel.syntax.given
+            |
+            |val strX: XcelValue      = str
+            |val dobX: XcelValue      = dob
+            |val intX: XcelValue      = int
+            |val longX: XcelValue     = long
+            |val boolX: XcelValue     = bool
+            |val dateX: XcelValue     = date
+            |val dateTimeX: XcelValue = dateTime
+            |val optStrX: XcelValue   = optStr
+            |val optDobX: XcelValue   = optDob
+            |
+            |XcelVals(str, dob, int, long, bool, date, dateTime, optStr, optDob)
+            |ScalaVals(strX, dobX, intX, longX, boolX, dateX, dateTimeX, optStrX, optDobX)
+            |""".stripMargin
+        )
+
+        errors shouldBe empty
     }
   }
 
-case class User(name: String, age: Option[Int])
+case class ScalaVals(
+  str: String,
+  dob: Double,
+  int: Int,
+  long: Long,
+  bool: Boolean,
+  date: LocalDate,
+  dateTime: LocalDateTime,
+  optStr: Option[String],
+  optDob: Option[Double]
+)
+
+case class XcelVals(
+  str: XcelValue,
+  dob: XcelValue,
+  int: XcelValue,
+  long: XcelValue,
+  bool: XcelValue,
+  date: XcelValue,
+  dateTime: XcelValue,
+  optStr: XcelValue,
+  optDob: XcelValue
+)
